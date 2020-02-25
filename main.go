f3bf6d656fa12e2e10380d065a8bebae4129d492
Mayowa Aj <sagaraaj69@gmail.com>
2020-02-25 01:52:28 +0100

    WIP: endpoints to get all books

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
-	"fmt"
+    "fmt"
+    "net/http"
+
+    "github.com/gorilla/mux"
 )
 
 func main() {
-	fmt.Println("Hello World")
+    r := mux.NewRouter()
+
+    r.HandleFunc("/books", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Should return a list of all books") //TODO implement
+	})
+	
+	fmt.Println("Listening on port 80...")
+    http.ListenAndServe(":80", r)
 }
